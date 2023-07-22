958436220fadb256ed15cea7ccba90571a3292b0
MHRezaie <rezaiee.mhr01@gmail.com>
2023-07-22 10:42:45 -0400

    Complete Front End part

---
+++ src/views/partials/footer.php
@@ -0,0 +1,8 @@
+        <footer class="footer mt-auto py-3 bg-secondary-subtle">
+        <div class="container">
+            <span class="text-body-secondary">تمامی حقوق محفوظ است.</span>
+        </div>
+        </footer>
+        <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
