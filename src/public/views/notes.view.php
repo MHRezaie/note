00a0b9b57ee5523009fc0c1b791dab424ad2ad4b
MHRezaie <rezaiee.mhr01@gmail.com>
2023-07-25 11:00:50 -0400

    Style notes table

--- src/public/views/notes.view.php
+++ src/public/views/notes.view.php
@@ -1,22 +1,24 @@
-<div class="container w-100">
-    <div class="pt-5 d-flex flex-column align-items-around">
-        <div class="d-flex justify-content-center">
+<div class="container">
+    <div class="pt-5 d-flex flex-column  w-75 mx-auto">
+        <div class="d-flex justify-content-center ">
             <div class="px-3 py-2 border-bottom border-top border-3 font-weight-bold">
             #
             </div>
-            <div class="w-75 py-2 px-5 text-center border-bottom  border-top border-3 font-weight-bold">
+            <div class="w-100 py-2 px-5 text-center border-bottom  border-top border-3 font-weight-bold">
                 متن
             </div>
         </div>
         <?php foreach($notes as $key=>$note):?>
+        <a class="btn btn-outline-warning  border-0 text-dark" href="/note/<?php echo $note["id"]?>">
             <div class="d-flex justify-content-center">
                 <div class="px-3 py-3 border-bottom ">
                     <?php echo $key+1?>
                 </div>
-                <div class="w-75 py-3 px-5 text-center border-bottom  ">
+                <div class="w-100 py-3 px-5 text-center border-bottom  ">
                     <?php echo $note["body"]?>
                 </div>
             </div>
+        </a>
         <?php endforeach;?>
     </div>
 </div>
