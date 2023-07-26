b33cc7b53c2757ad3bf4a141ff5f5f1d9c8b4437
MHRezaie <rezaiee.mhr01@gmail.com>
2023-07-26 12:19:23 -0400

    Add back button to create note page

--- src/public/views/noteCreate.view.php
+++ src/public/views/noteCreate.view.php
@@ -3,6 +3,10 @@
     <form class="d-flex flex-column w-75 mx-auto">
             <label class="text-success mb-2" for="exampleFormControlTextarea1">یادداشت جدید</label>
             <textarea maxlength="1023" class="form-control w-100" id="exampleFormControlTextarea1" rows="10"></textarea>
-            <button type="submit" class="btn btn-primary mt-3 px-5 py-1 align-self-end ">ایجاد</button>
+            <div class="btn-row align-self-end">
+                <a href="/notes" class="btn btn-secondary mt-3 px-4 py-1 ">بازگشت</a>
+                <button type="submit" class="btn btn-primary mt-3 px-5 py-1 ">ایجاد</button>
+            </div>
+            
     </form>
 </div>
