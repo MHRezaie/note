e2a14552f82b47e33336296de95117d66c35d1a7
MHRezaie <rezaiee.mhr01@gmail.com>
2023-07-26 12:46:58 -0400

    make create note responsive

--- src/public/views/noteCreate.view.php
+++ src/public/views/noteCreate.view.php
@@ -1,12 +1,11 @@
 
 <div class="container " >
-    <form class="d-flex flex-column w-75 mx-auto">
-            <label class="text-success mb-2" for="exampleFormControlTextarea1">یادداشت جدید</label>
-            <textarea maxlength="1023" class="form-control w-100" id="exampleFormControlTextarea1" rows="10"></textarea>
-            <div class="btn-row align-self-end">
-                <a href="/notes" class="btn btn-secondary mt-3 px-4 py-1 ">بازگشت</a>
-                <button type="submit" class="btn btn-primary mt-3 px-5 py-1 ">ایجاد</button>
-            </div>
-            
+    <form class="d-flex flex-column mx-auto">
+        <label class="text-success mb-2" for="exampleFormControlTextarea1">یادداشت جدید</label>
+        <textarea maxlength="1023" class="form-control w-100" id="exampleFormControlTextarea1" rows="10"></textarea>
+        <div class="d-flex flex-row-reverse align-self-center ">
+              <button type="submit" class="btn btn-primary mt-3 px-4 py-1 ">ایجاد</button>
+                <a href="/notes" class="btn btn-secondary mt-3 px-2 py-1 mx-1">بازگشت</a>
+        </div>
     </form>
 </div>
