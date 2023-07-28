ad86a727b666698dd2ba7973621cd51e1020bca9
MHRezaie <rezaiee.mhr01@gmail.com>
2023-07-28 14:15:07 -0400

    Add backend validation before storing notes

--- src/public/controllers/noteCreate.php
+++ src/public/controllers/noteCreate.php
@@ -1,18 +1,27 @@
 <?php
 require("DataBase.php");
+$errors=[];
+$noteBody="";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $noteBody=$_POST["noteBody"];
+    $noteBody=$_POST['noteBody'];
     $conf=require("config.php");
     $db=new DataBase($conf["database"],'admin','secret');
+    
     if(isset($noteBody)){
+        $errors[]="متن نمی‌تواند خالی باشد.";
+    }
+    if(strlen($noteBody)>1023){
+        $errors[]="طول متن بیشتر از حد مجاز(۱۰۲۳ کاراکتر) است.";
+    }
+    if(!$errors){
         $db->query("insert into notes(body,user_id) values (:body, :user_id)",
             [
                 "body"=>htmlspecialchars($noteBody),
                 "user_id"=>userId()
             ]
         );
+        header('location: /notes');
     }
-    header('location: /notes');
 }
 
 
