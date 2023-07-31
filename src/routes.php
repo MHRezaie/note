29d963992dff6903a6400142b512d18ed638291d
MHRezaie <rezaiee.mhr01@gmail.com>
2023-07-31 02:28:26 -0400

    add buttons for update and delete in note.view.php

--- src/routes.php
+++ src/routes.php
@@ -12,10 +12,12 @@
 $router->get("/","controllers/index.php");
 $router->get("/about","controllers/about.php");
 $router->get("/contact","controllers/contact.php");
+
 $router->get("/notes","controllers/notes/index.php");
-$router->get("/note","controllers/notes/note.php");
 $router->get("/notes/create","controllers/notes/create.php");
 $router->post("/notes","controllers/notes/store.php");
+
+$router->get("/note","controllers/notes/note.php");
 //     "/about"=>"controllers/about.php",
 //     "/contact"=>"controllers/contact.php",
 //     "/notes"=>"controllers/notes/index.php",
--- src/views/notes/note.view.php
+++ src/views/notes/note.view.php
@@ -2,7 +2,7 @@
 require base_path("views/partials/head.php");
 require base_path("views/partials/nav.php");
 ?>
-<div class="pt-5 d-flex flex-column  w-75 mx-auto">
+<div class="pt-5 d-flex flex-column col-md-8  mx-auto">
     <div class="d-flex pb-2 justify-content-end">
             <a href="/notes" class="btn text-primary border-0">
                 بازگشت
@@ -18,12 +18,25 @@ require base_path("views/partials/nav.php");
     </div>
     
     <div class="d-flex justify-content-center">
-        <div class="px-3 py-3 border-bottom ">
-            <?php echo $note["id"]?>
+        <div class="px-3 py-5 ">
+            <p class="py-3"><?php echo $note["id"]?></p>
         </div>
-        <div class="w-100 py-3 text-center border-bottom  ">
-            <?php echo $note["body"]?>
+        <div class="w-100 py-5 text-center ">
+            <form method="POST" id="note-textarea" >
+                <input type="hidden" name="_method" value="PUT">
+                <input type="hidden" name="id" value="<?php echo $note["id"]; ?>">
+                <textarea  id="noteBody" name="noteBody" class="py-3 w-100 text-center border-3 border-secondary-subtle " rows="4"><?php echo $note["body"]?></textarea>
+            </form>
         </div>
+        
+    </div>
+    <div class="d-flex justify-content-md-end justify-content-center ">
+        <form method="POST" class=" mx-1" >
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="id" value="<?php echo $note["id"]; ?>">
+            <input type="submit" class="btn btn-danger px-3" value="حذف" />
+        </form>
+        <input type="submit" form="note-textarea" class="btn btn-success px-3 mx-1"  value="ویرایش">
     </div>
 </div>
 
