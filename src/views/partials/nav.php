4f20fe2dc65cd0938dac91a375163778fd502331
MHRezaie <rezaiee.mhr01@gmail.com>
2023-08-05 10:00:05 -0400

    Add logout front end

--- src/views/partials/nav.php
+++ src/views/partials/nav.php
@@ -6,7 +6,7 @@
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
+      <div class="collapse navbar-collapse py-2" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
             <a class="nav-link <?php echo isActive("/");?>" aria-current="page" href="/">خانه</a>
@@ -21,10 +21,17 @@
             <a class="nav-link <?php echo isActive("/notes");?>" href="/notes">یادداشت</a>
           </li>
         </ul>
-        <div class="" class="login">
-          <a class="btn btn-outline-warning" href="/sessions" >ورود</a>
-          <a class="btn btn-outline-success mx-2" href="/register" >عضویت</a>
-        </div>
+        <?php if(isset($_SESSION['user'])):?>
+          <form action="/sessions" method="POST" >
+            <input type="hidden" name="_method" value="DELETE">
+            <button type="submit" class="btn btn-outline-danger ligh">خروج از حساب</button>
+          </form>
+        <?php else:?>
+          <div class="" class="login">
+            <a class="btn btn-outline-warning" href="/sessions" >ورود</a>
+            <a class="btn btn-outline-success mx-2" href="/register" >عضویت</a>
+          </div>
+        <?php endif;?>
       </div>
     </div>
   </nav>
